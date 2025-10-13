ed0d8eeffac2e627fd717a05164de9c8a293063d
jcanales <jcanales@webdoxclm.com>
2025-10-12 21:21:22 -0300

    feat: create method to send form data

--- app/jobs/create_order_job.rb
+++ app/jobs/create_order_job.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class CreateOrderJob < ApplicationJob
+  class WebpayTransactionError < StandardError; end
   ORDER_URL = ENV.fetch("ORDER_URL", "http://localhost:8080/orders")
+  NOTIFY_URL = ENV.fetch("NOTIFY_URL", "http://localhost:4000/api/v1/payments")
   queue_as :orders
 
   limits_concurrency to: 5, key: ->(user_id, _) { "user_orders_#{user_id}" }, duration: 5.minutes
@@ -13,13 +15,13 @@ class CreateOrderJob < ApplicationJob
     return unless User.find_by(id: @user_id).present?
 
     response_data = create_order(order_params)
+    Rails.logger.info("Order #{response_data['order_id']} processed in Go service, payment initialized")
     @order_id = response_data["order_id"]
     @total_amount = response_data["total"].to_i
-
     @payment = create_pending_payment
     create_webpay_transaction
-
-    Rails.logger.info("Order #{@order_id} processed in Go service, payment initialized")
+    notify_payment
+    Rails.logger.info("Order #{@order_id} and payment #{@payment.id} created successfully")
   rescue RestClient::ExceptionWithResponse => e
     Rails.logger.error("Go service error: #{e.response}")
     raise
@@ -44,7 +46,32 @@ class CreateOrderJob < ApplicationJob
 
     raise "Invalid response from Go service" unless response.code == 200
 
-    JSON.parse(response.body)
+    response = response.body
+    Rails.logger.info("Response from Go service: #{response}")
+    JSON.parse(response)
+  end
+
+  def notify_payment
+    Rails.logger.info("Notifying payment for order #{@order_id}")
+    data = JSON.parse(@payment.transaction_data || "{}")
+    raise "Missing transaction data for payment #{@payment.id}" if data["token"].blank? || data["url"].blank?
+
+    payload = {
+      payment: {
+        id: @payment.id,
+        user_id: @user_id
+      }
+    }
+
+    response = RestClient.post(
+      NOTIFY_URL, payload.to_json,
+      { content_type: :json, accept: :json }
+    )
+    raise "Invalid response from Elixir service" unless response.code == 200
+
+    response = response.body
+    Rails.logger.info("Response from Elixir service: #{response}")
+    JSON.parse(response)
   end
 
   def create_pending_payment
@@ -70,5 +97,8 @@ class CreateOrderJob < ApplicationJob
       transaction_token: create_tx["token"],
       transaction_data: create_tx.to_json
     )
+  rescue WebpayTransactionError => e
+    Rails.logger.error("Error creating Webpay transaction: #{e.message}")
+    raise
   end
 end
