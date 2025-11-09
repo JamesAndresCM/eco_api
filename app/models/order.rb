4db1b7082768bc4901ea43c0c110401f066077e7
jcanales <jcanales@webdoxclm.com>
2025-11-08 22:22:24 -0300

    feat: update order job and decrement stock process

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :items, class_name: "OrderItem", dependent: :destroy
-  has_one :payment
   has_many :inventory_movements, foreign_key: :related_order_id
   enum :status, { pending: 0, completed: 1, cancelled: 2 }, prefix: true
 end
