c93b344ae6b6eace39531ce1083db69bca00e1f7
jcanales <jcanales@webdoxclm.com>
2026-02-01 14:11:33 -0300

    feat: remove securerandom

--- app/services/payments/idempotent_payment_service.rb
+++ app/services/payments/idempotent_payment_service.rb
@@ -36,7 +36,7 @@ module Payments
     attr_reader :idempotency_key, :order_id, :user_id, :amount
 
     def generate_idempotency_key
-      "payment:order:#{order_id}:user:#{user_id}:#{SecureRandom.hex(4)}"
+      "payment:order:#{order_id}:user:#{user_id}"
     end
 
     def create_payment_safely
